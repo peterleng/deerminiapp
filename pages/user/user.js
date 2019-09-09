4933c9e17bb7ace7379019bd9ab8a25ecaa3b116
leng <lyf@neuron.sg>
2019-09-10 00:24:52 +0800

    profile UI
    profile UI

--- pages/user/user.js
+++ pages/user/user.js
@@ -1,18 +1,78 @@
 // pages/user/index.js
-Page({
+const app = getApp()
 
+Page({
   /**
    * 页面的初始数据
    */
   data: {
-
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    groups: [
+      {
+        title: '',
+        rows: [
+          {
+            image: '../../images/company.png',
+            title: '团队管理',
+            size: 40,
+          },
+          {
+            image: '../../images/department.png',
+            title: '部门管理',
+            size: 40,
+          },
+        ]
+      },
+      {
+        title: '',
+        rows: [
+          {
+            image: '../../images/add_member.png',
+            title: '新增员工',
+            size: 56,
+          },
+          {
+            image: '../../images/group.png',
+            title: '员工管理',
+            size: 40,
+          },
+        ]
+      },
+    ],
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
   },
 
   /**
@@ -62,5 +122,13 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+  getUserInfo: function (e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
   }
 })
